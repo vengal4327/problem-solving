5732eada022dcdc671452c8b12a28f25a3b4e800
vengal <vengal.learn2code@gmail.com>
2022-01-10 17:27:17 -0600

    Create RemoveKDigits.java

---
+++ RemoveKDigits.java
@@ -0,0 +1,35 @@
+class Solution {
+  public String removeKdigits(String num, int k) {
+      
+   //Examples-- "100280" and 1
+   //   "1432219", k = 3 --> Output-1219
+      
+    LinkedList<Character> stack = new LinkedList<Character>();
+        
+    for(char digit : num.toCharArray()) {
+      while(stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+        stack.removeLast();
+        k -= 1;
+      }
+      stack.addLast(digit);
+    }
+        
+    /* remove the remaining digits from the tail. */
+    for(int i=0; i<k; ++i) {
+      stack.removeLast();
+    }
+        
+    // build the final string, while removing the leading zeros.
+    StringBuilder ret = new StringBuilder();
+    boolean leadingZero = true;
+    for(char digit: stack) {
+      if(leadingZero && digit == '0') continue;
+      leadingZero = false;
+      ret.append(digit);
+    }
+        
+    /* return the final string  */
+    if (ret.length() == 0) return "0";
+    return ret.toString();
+  }
+}
